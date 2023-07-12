b58788bf8d1334e463304ddc149feb371032cfbb
Theodor F. Pöschl <125547652+theo-felix-poeschl@users.noreply.github.com>
2023-07-12 09:50:35 +0200

    Add progress bar to trivial_pdclust

--- pdclust_py/pdclust_py/core.py
+++ pdclust_py/pdclust_py/core.py
@@ -1,19 +1,20 @@
-import os
-import itertools
+from pathlib import Path
 import numba
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from pathlib import Path
 import multiprocessing as mp
-
-from pathlib import Path
 from tqdm import tqdm
-
 
 @numba.jit(nopython=True)
 def numba_calc_pdist(pos_arr1, pos_arr2, meth_arr1, meth_arr2):
+    """
+    Calculate the pairwise distance between two cells
+    :param pos_arr1: Array of starting positions of CpGs in the first cell
+    :param pos_arr2: Array of starting positions of CpGs in the second cell
+    :param meth_arr1: Array of methylation values in the first cell
+    :param meth_arr2: Array of methylation values in the second cell
+    :return: pair_dist = pairwise distance, number_of_cpgs = Number of CpGs that are present in both cells
+    """
     idx1 = 0
     idx2 = 0
     number_of_cpgs = 0
@@ -32,6 +33,11 @@ def numba_calc_pdist(pos_arr1, pos_arr2, meth_arr1, meth_arr2):
 
 
 def numba_get_pairwise_dissimilarity(args):
+    """
+    Open the bed files and pass the data to numba_calc_pdist to calculate the pairwise distance
+    :param args: Tuple of two bed file paths
+    :return: row_label = name of cell 1, col_label = name of cell 2, pairwise_dissimilarity
+    """
     total_cpgs = 0
     pairwise_dissimilarity = 0
     row_label, col_label = args
@@ -50,8 +56,12 @@ def numba_get_pairwise_dissimilarity(args):
     return row_label, col_label, (pairwise_dissimilarity / total_cpgs)
 
 
-def numba_pdclust(path_to_bed_files, plot=True):
-    results = []
+def numba_pdclust(path_to_bed_files):
+    """
+    Calculate the pairwise dissimilarity between all cells in the given directory
+    :param path_to_bed_files: Directory containing the bed files
+    :return: Pandas DataFrame containing all pairwise dissimilarities in square form
+    """
     path_to_bed_files = Path(path_to_bed_files)
     file_list = list(path_to_bed_files.glob("*.bed"))
     with mp.Pool(processes=mp.cpu_count()) as pool:
@@ -60,13 +70,20 @@ def numba_pdclust(path_to_bed_files, plot=True):
 
     index = columns = [file.stem for file in file_list]
     result_df = pd.DataFrame(index=index, columns=columns)
-
-    for i, elem in enumerate(results):
+    for elem in results:
         elem1, elem2, pairwise_dissimilarity = elem
         result_df.at[elem1, elem2] = pairwise_dissimilarity
-        
-    result_df.fillna(0.0, inplace=True)
-    
+    result_df['number'] = None
+    result_df['type'] = None
+    result_df['type'] = [x.split('_')[0]for x in result_df.index.to_list()]
+    for i, x in enumerate(result_df.index.to_list()):
+        if len(x.split('_')) == 2:
+            result_df['number'].iloc[i] = '999'
+        else:
+            result_df['number'].iloc[i] = x.split('_')[3]
+    result_df = result_df.sort_values(by=['type', 'number'])
+    result_df = result_df[result_df.index.to_list()]
+
     return result_df
 
 
@@ -74,20 +91,13 @@ def numba_pdclust(path_to_bed_files, plot=True):
 ############# end numba #################
 #########################################
 
-import numpy as np
-import os
-import tqdm
-import seaborn as sns
-import datetime
-import multiprocessing as mp
-
-
-import numpy as np
-import multiprocessing as mp
-
-
-def get_pairwise_dissimilarity(args):
-    row_label, col_label = args
+def get_pairwise_dissimilarity(tuple_of_bed_file_paths):
+    """
+    Open the bed files and calculate the pairwise distance
+    :param tuple_of_bed_file_paths: Tuple of two bed file paths
+    :return: row_label = name of cell 1, col_label = name of cell 2, pairwise_dissimilarity
+    """
+    row_label, col_label = tuple_of_bed_file_paths
     df1 = pd.read_csv(row_label, sep='\t', usecols=(0, 1, 3), names=['chr', 'pos', 'frac_methyl'])
     df2 = pd.read_csv(col_label, sep='\t', usecols=(0, 1, 3), names=['chr', 'pos', 'frac_methyl'])
     row_label, col_label = Path(row_label).stem, Path(col_label).stem
@@ -98,20 +108,32 @@ def get_pairwise_dissimilarity(args):
     return row_label, col_label, pairwise_dissimilarity
 
 
-def trivial_pdclust(path_to_bed_files, plot=True):
+def trivial_pdclust(path_to_bed_files):
+    """
+    :param path_to_bed_files: Path to directory containing the bed files
+    :return: Pandas DataFrame containing all pairwise dissimilarities in square form
+    """
     path_to_bed_files = Path(path_to_bed_files)
-    file_list = list(path_to_bed_files.glob("*.bed"))
+    file_list = sorted(list(path_to_bed_files.glob("*.bed")))
 
     with mp.Pool(processes=mp.cpu_count()) as pool:
-        results = pool.map(get_pairwise_dissimilarity, [(elem1, elem2) for elem1 in file_list for elem2 in file_list if elem1 != elem2])
+        results = list(tqdm(pool.imap(get_pairwise_dissimilarity, [(elem1, elem2) for elem1 in file_list for elem2 in file_list if elem1 != elem2]), total=len(file_list) * (len(file_list) - 1) / 2))
 
     index = columns = [file.stem for file in file_list]
     result_df = pd.DataFrame(index=index, columns=columns)
-
     for elem in results:
         elem1, elem2, pairwise_dissimilarity = elem
         result_df.at[elem1, elem2] = pairwise_dissimilarity
-
-    result_df.fillna(0.0, inplace=True)
+    result_df['number'] = None
+    result_df['type'] = None
+    result_df['type'] = [x.split('_')[0]for x in result_df.index.to_list()]
+    for i, x in enumerate(result_df.index.to_list()):
+        if len(x.split('_')) == 2:
+            result_df['number'].iloc[i] = '999'
+        else:
+            result_df['number'].iloc[i] = x.split('_')[3]
+    result_df = result_df.sort_values(by=['type', 'number'])
+    result_df = result_df[result_df.index.to_list()]
 
     return result_df
+
